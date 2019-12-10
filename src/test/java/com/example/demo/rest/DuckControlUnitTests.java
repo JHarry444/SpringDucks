1c2fe8d86e450acef02446b3607aa934869aaf24
jharry444 <jordan.harrison@qa.com>
2019-12-10 16:33:30 +0000

    Updated to junit 5

--- src/test/java/com/example/demo/rest/DuckControlUnitTests.java
+++ src/test/java/com/example/demo/rest/DuckControlUnitTests.java
@@ -1,7 +1,6 @@
-package com.example.demo.service;
+package com.example.demo.rest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.times;
 
 import java.util.ArrayList;
@@ -14,26 +13,35 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.demo.persistence.domain.Duck;
-import com.example.demo.persistence.repo.DuckRepo;
+import com.example.demo.rest.DuckController;
+import com.example.demo.service.DuckService;
 
 @SpringBootTest
-public class DuckServiceUnitTests {
+public class DuckControlUnitTests {
 
 	@InjectMocks
-	private DuckService service;
-
+	private DuckController controller;
+	
 	@Mock
-	private DuckRepo repo;
-
+	private DuckService service;
+	
 	@Test
 	public void getAllDucksTest() {
 		List<Duck> pond = new ArrayList<>();
 		pond.add(new Duck("Ducktor Doom", "Grey", "Latveria"));
+		Mockito
+			.when(this.service.readDucks())
+			.thenReturn(pond);
 		
-		Mockito.when(repo.findAll()).thenReturn(pond);
+		assertEquals(false, this.controller.getAll().isEmpty());
 		
-		assertEquals(false, this.service.readDucks().isEmpty());
-
-		Mockito.verify(repo, times(1)).findAll();
+		Mockito.verify(service, times(1)).readDucks();
 	}
 }
+
+
+
+
+
+
+
