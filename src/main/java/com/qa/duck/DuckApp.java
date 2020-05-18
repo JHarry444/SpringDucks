4fb9beba5018f74dcb596803cc38ab9bba40cacb
Jordan <jordan.harrison@qa.com>
2020-05-18 11:04:33 +0100

    Added some Swagger

--- src/main/java/com/qa/duck/DuckApp.java
+++ src/main/java/com/qa/duck/DuckApp.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class DuckApp extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
