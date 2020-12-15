95d2dd3152b28b5ae0025b8edb766061896b3503
jharry444 <jordan.harrison@qa.com>
2020-12-15 10:13:43 +0000

    updated dependencies

--- src/main/java/com/qa/duck/DuckApp.java
+++ src/main/java/com/qa/duck/DuckApp.java
@@ -2,13 +2,9 @@ package com.qa.duck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
-public class DuckApp extends SpringBootServletInitializer {
+public class DuckApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DuckApp.class, args);
