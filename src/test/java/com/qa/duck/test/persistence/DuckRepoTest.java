988a61391fa2912428bbe33f43079f1c1ff24fa1
jharry444 <jordan.harrison@qa.com>
2021-03-17 10:54:03 +0000

    updated jpaTest to not modify the repo directly

--- src/test/java/com/qa/duck/test/persistence/DuckRepoTest.java
+++ src/test/java/com/qa/duck/test/persistence/DuckRepoTest.java
@@ -2,38 +2,34 @@ package com.qa.duck.test.persistence;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 //Only needed to test custom repo methods (especially when written with @query)
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
 import com.qa.duck.persistence.domain.Duck;
 import com.qa.duck.persistence.repo.DuckRepo;
 
 @DataJpaTest
+@Sql(scripts = { "classpath:test-schema.sql",
+		"classpath:test-data.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@ActiveProfiles("test")
 class DuckRepoTest {
 
 	@Autowired
 	private DuckRepo repo;
 
-	private final String TEST_COLOUR = "black";
-
-	private final Duck TEST_DUCK = new Duck("Duck Dodger", TEST_COLOUR, "The 24th and a half century");
-
-	private Duck testSavedDuck;
-
-	@BeforeEach
-	void init() {
-		this.repo.deleteAll();
-		this.testSavedDuck = this.repo.save(this.TEST_DUCK);
-	}
-
 	@Test
 	void testFindByName() {
-		assertThat(this.repo.findByColour(this.TEST_COLOUR)).containsExactly(this.testSavedDuck);
+		final String colour = "blue";
+
+		final Duck testDuck = new Duck(1L, "Barry", colour, "pub", 0);
+		assertThat(this.repo.findByColour(colour)).containsExactly(testDuck);
 	}
 
 }
